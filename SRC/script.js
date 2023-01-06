881c76208b2c0306afa29d637afaf2ee25797fd3
Kaptainkiely <102674167+Kaptainkiely@users.noreply.github.com>
2023-01-06 20:06:35 +0000

    add javascript to project. made mobile friendly

--- SRC/script.js
+++ SRC/script.js
@@ -1,2 +1,24 @@
-const game = document.qetElementById('game')
-const scoreDisplay = document.getElementById('score')
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#ScoreText');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter =0
+let availableQuestions = []
+
+let questions = [
+    {
+       question: 'which continent is india in ?',
+       choice1: 'Europe',
+       choice2: 'Asia',
+       choice3: 'South America',
+       choice4: 'Africa',
+       answer: Asia,
+
+
+    }
+]
